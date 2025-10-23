54f98fdd167a1a5d74106e6cf1c3fa4397c4e52b
Aadi-patel04 <aadipatel0042@gmail.com>
2025-10-23 23:54:09 +0530

    Navbar and fix changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function HeroSection() {
     return (
+        
         <div
             className='h-auto md:h-[40rem] w-full rounded-md flex
     items-center justify-center relative overflow-hidden mx-auto py-10
